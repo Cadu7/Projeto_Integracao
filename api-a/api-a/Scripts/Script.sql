0cbf01d87ed2ff76382430b3e1cc40d4be2ffe4a
Cadu7 <cadumdecarvalho@gmail.com>
2022-04-08 12:36:43 -0300

    .

---
+++ api-a/api-a/Scripts/Script.sql
@@ -0,0 +1,32 @@
+INSERT INTO Sensitized_Event values
+(1,"venda sem cupom", "Barramento", 50, "2554652", 45, "L125", "2021-09-05 12:32:02", "PDV_SALE"),
+(2,"venda sem cupom", "Barramento", 50, "2554635", 45, "L125", "2021-09-05 12:32:02", "PDV_SALE"),
+(3,"venda sem cupom", "Barramento", 45, "2554635", 50, "L125", "2021-09-05 12:32:02", "PDV_SALE"),
+(4,"venda sem cupom", "Barramento", 45, "2554635", 50, "L018", "2021-10-05 12:40:02", "PDV_SALE_CANC"),
+(5,"venda com cupom", "Barramento", 45, "2554656", 50, "L018", "2021-10-10 12:40:02", "PDV_SALE_CANC"),
+(6,"venda com cupom", "Barramento", 50, "2554656", 45, "L018", "2021-10-10 12:40:02", "PDV_SALE_CANC"),
+(7,"venda com cupom", "Barramento", 50, "2554652", 45, "L125", "2021-10-05 12:40:02", "PDV_SALE_CANC"),
+(8,"", "Maestro", 500, "2598652", 60, "L815", "2021-07-06 12:40:02", "O2O_CONFIRMED_ORDER"),
+(9,"", "Maestro", 500, "2598652", 60, "L815", "2021-07-06 12:40:02", "O2O_CONFIRMED_ORDER"),
+(10,"", "Maestro", 500, "2598652", 60, "L895", "2021-09-06 12:40:02", "O2O_CANCELED_ORDER"),
+(11,"", "Maestro", 500, "2598652", 57, "L895", "2021-09-06 12:40:02", "O2O_CANCELED_ORDER"),
+(12,"", "Maestro", 98, "2598652", 57, "L895", "2021-09-06 12:40:02", "O2O_CANCELED_ORDER"),
+(13,"", "Maestro", 98, "2598652", 57, "L895", "2021-09-06 12:40:02", "O2O_CANCELED_ORDER"),
+(14,"", "Maestro", 98, "2598652", 57, "L815", "2021-08-06 12:40:02", "O2O_CANCELED_ORDER"),
+(15,"", "Maestro", 98, "2598652", 60, "L815", "2021-08-06 12:40:02", "O2O_CONFIRMED_ORDER");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
